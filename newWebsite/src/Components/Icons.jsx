c686c26c728c6cc005d674aa175c5cfbd016799f
MuhammadHaseeb1285 <haseebarif1285@gmail.com>
2025-05-03 17:57:57 +0500

    changes in icons

--- newWebsite/src/Components/Icons.jsx
+++ newWebsite/src/Components/Icons.jsx
@@ -1,10 +1,22 @@
+import { FaSun, FaHippo, FaMountain, FaWater, FaSnowflake, FaIcicles } from 'react-icons/fa';
 import * as FaIcons from 'react-icons/fa';
 
-// Export an array of at least 100 unique FontAwesome icons
-export const iconList = Object.entries(FaIcons)
-  .filter(([name]) => name.startsWith('Fa'))
-  .slice(0, 200)
-  .map(([name, IconComponent]) => ({ icon: <IconComponent />, name }));
+// Use SVG icons for sun, lion, mountains, sea, snow, and ice, styled black
+const customIcons = [
+  { icon: <FaSun color="black" />, name: 'Sun' },
+  { icon: <FaHippo color="black" />, name: 'Lion' }, // FaHippo as closest animal
+  { icon: <FaMountain color="black" />, name: 'Mountains' },
+  { icon: <FaWater color="black" />, name: 'Sea' },
+  { icon: <FaSnowflake color="black" />, name: 'Snow' },
+  { icon: <FaIcicles color="black" />, name: 'Ice' },
+];
 
-// Optionally, export a default icon for initial use
+export const iconList = [
+  ...customIcons,
+  ...Object.entries(FaIcons)
+    .filter(([name]) => name.startsWith('Fa'))
+    .slice(0, 200)
+    .map(([name, IconComponent]) => ({ icon: <IconComponent />, name })),
+];
+
 export const DefaultIcon = iconList[0].icon;
